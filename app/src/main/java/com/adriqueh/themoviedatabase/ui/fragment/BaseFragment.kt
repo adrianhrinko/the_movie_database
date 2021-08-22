c35dfac7826e7608b43008359875aa5e600db924
Adrian Hrinko <adrian.hrinko@safetica.com>
2021-08-22 23:48:35 +0200

    added missing dependency

--- app/src/main/java/com/adriqueh/themoviedatabase/ui/fragment/BaseFragment.kt
+++ app/src/main/java/com/adriqueh/themoviedatabase/ui/fragment/BaseFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
